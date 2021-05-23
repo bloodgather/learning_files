08607952814a53f7d0de1da627a5af72ca280112
unknown <1184030745@qq.com>
2021-05-23 18:02:16 +0800

    new_file_5.23

---
+++ java代码/HelloWorld.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+public class IfDemo {
+	public static void main(String[] args){
+		Random r = new Random();
+		int num = r.nextInt(10);
+		System.out.println(num);
+	} 
+}
+	
---
+++ java代码/src/joe_01_package/HelloWorld.java
@@ -0,0 +1,7 @@
+package joe_01_package;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
